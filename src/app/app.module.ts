ee5ee3c67b4b1871e456d4632efbcdf0ce9faa95
rohitpim1 <pimrohit33@gmail.com>
2022-03-01 11:34:36 +0530

    4 files modified, 10 added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-
+import {HeaderComponent} from './header/header.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {FormdataComponent} from './formdata/formdata.component';
+import {FooterComponent} from './footer/footer.component';
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent,HeaderComponent,NavbarComponent,FormdataComponent,FooterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/formdata/formdata.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formdata',
+  templateUrl: './formdata.component.html',
+  styleUrls: ['./formdata.component.css']
+})
+export class FormdataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
